9d2870daf739669b9b3180b7595414387a3836b3
yotamroshuji <yotam.rosenmann@mail.huji.ac.il>
2022-09-06 01:20:51 +0300

    Made the course collector collect only the specific courses requested by year and semester, and added compatibility in the deploy.yml

--- collect_courses.py
+++ collect_courses.py
@@ -8,15 +8,19 @@ def create_courses_json():
     parser = argparse.ArgumentParser()
     parser.add_argument('course_file_directory')
     parser.add_argument('output_file_dir')
-    parser.add_argument('year')
+    parser.add_argument('output_year')
+    parser.add_argument('collected_file_year')
     parser.add_argument('semester')
     args = parser.parse_args()
 
     file_list = os.listdir(args.course_file_directory)
-    semester_number = '01' if args.semester == 'a' else '02'
-    output_file_path = os.path.join(args.output_file_dir, f'courses_{args.year}{semester_number}.min.js')
+    semester_number = '1' if args.semester == 'a' else '2'
+    filtered_file_list = [file_name
+                          for file_name in file_list
+                          if file_name.endswith(f'{args.collected_file_year}_{semester_number}.txt')]
+    output_file_path = os.path.join(args.output_file_dir, f'courses_{args.output_year}0{semester_number}.min.js')
     all_courses = []
-    for course_file_name in file_list:
+    for course_file_name in filtered_file_list:
         course_file_path = os.path.join(args.course_file_directory, course_file_name)
         with open(course_file_path, 'r') as f:
             all_courses.append(json.load(f))
